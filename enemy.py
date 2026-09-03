2e13267768beb5facd32944bff195f57fefa2dd6
Tilpx <johanneskuhl987@gmail.com>
2026-09-03 12:18:52 +0200

    enemy toegevoegd en circle collision

---
+++ enemy.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+def enemymovement(clock, player_position, enemy_position, enemy_speed, enemy_rect):
+    dt = clock.tick(60) / 1000
+
+    enemy_direction = player_position - enemy_position
+
+    if enemy_direction.length_squared() > 0:
+        enemy_direction.normalize_ip()
+
+    enemy_position += enemy_direction * enemy_speed * dt
+
+    enemy_rect.center = (round(enemy_position.x), round(enemy_position.y))
+
+    return enemy_rect
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pygame
 from player import (
     WASDmovement
 )
+
+from enemy import (
+    enemymovement
+)
 pygame.init()
 
 
@@ -15,14 +19,19 @@ PLAYER_SIZE = PLAYER_RADIUS * 2
 x = WIDTH // 2
 y = HEIGHT // 2
 
-
+# PLAYER
 player_surf = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
 pygame.draw.circle(player_surf, "White", (PLAYER_RADIUS, PLAYER_RADIUS), PLAYER_RADIUS)
 player_rect = player_surf.get_rect(center=(x, y))
-
 player_position = pygame.Vector2(player_rect.center)
 player_speed = 300 # p/s
 
+# ENEMY
+enemy_surf = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
+pygame.draw.circle(enemy_surf, "Blue", (PLAYER_RADIUS, PLAYER_RADIUS), PLAYER_RADIUS)
+enemy_rect = enemy_surf.get_rect(center=(0, y))
+enemy_position = pygame.Vector2(enemy_rect.center)
+enemy_speed = 250 # p/s
 
 while True:
     #EVENT LOOP
@@ -34,8 +43,17 @@ while True:
 
     player_rect = WASDmovement(clock, player_speed, player_rect, player_position, WIDTH, HEIGHT)
 
+    enemy_rect = enemymovement(clock, player_position, enemy_position, enemy_speed, enemy_rect)
+
+    distance = player_position.distance_to(enemy_position)
+    if distance <= PLAYER_RADIUS * 2:
+        pygame.quit()
+        exit()
+
+    
     screen.fill((0, 0, 0))
     screen.blit(player_surf, player_rect)
+    screen.blit(enemy_surf, enemy_rect)
     pygame.display.update()
     
 
