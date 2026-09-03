28757f2e86898ede9031a80654a4437a08b33d65
Tilpx <johanneskuhl987@gmail.com>
2026-09-03 11:54:54 +0200

    changed all player movement from main to player.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 
+from player import (
+    WASDmovement
+)
 pygame.init()
 
 
@@ -28,33 +31,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    dt = clock.tick(60) / 1000
 
-    keys = pygame.key.get_pressed()
-    direction = pygame.Vector2(0, 0)
-
-    if keys[pygame.K_w]:
-        direction.y -= 1
-    if keys[pygame.K_a]:
-        direction.x -= 1
-    if keys[pygame.K_s]:
-        direction.y += 1
-    if keys[pygame.K_d]:
-        direction.x += 1
-
-    if direction.length_squared() > 0:
-        direction.normalize_ip()
-
-    player_position += direction * player_speed * dt
-
-    half_width = player_rect.width / 2
-    half_height = player_rect.height / 2
-
-    player_position.x = max(half_width, min(WIDTH - half_width, player_position.x))
-    player_position.y = max(half_height, min(HEIGHT - half_height, player_position.y))
-
-    player_rect.center = (round(player_position.x), round(player_position.y))
-
+    player_rect = WASDmovement(clock, player_speed, player_rect, player_position, WIDTH, HEIGHT)
 
     screen.fill((0, 0, 0))
     screen.blit(player_surf, player_rect)
