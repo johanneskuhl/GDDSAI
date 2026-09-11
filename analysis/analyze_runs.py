713f7fdcadca4060a24712b429253cbfc727b376
Tilpx <johanneskuhl987@gmail.com>
2026-09-11 09:48:46 +0200

    analysis fixen

--- analysis/analyze_runs.py
+++ analysis/analyze_runs.py
@@ -1,26 +1,28 @@
-import csv
+import csv  
+
+import os
 
 survivaltime_list = []
 
+if os.path.isfile("data/runs.csv"):
+    with open("data/runs.csv", "r") as runs:
+        csvreader = csv.reader(runs)
 
-with open("data/runs.csv", "r") as runs:
-    csvreader = csv.reader(runs)
+        next(csvreader)
 
-    next(csvreader)
+        for row in csvreader:
+            survivaltime_list.append(float(row[0]))
 
-    for row in csvreader:
-        survivaltime_list.append(float(row[0]))
+    if len(survivaltime_list) == 0:
+        print("no attempts made yet")
+    else:
+        pogingen = len(survivaltime_list)
+        gemiddelde_st = sum(survivaltime_list) / pogingen
+        min_st = min(survivaltime_list)
 
-if len(survivaltime_list) == 0:
-    print("no attempts made yet")
-else:
-    pogingen = len(survivaltime_list)
-    gemiddelde_st = sum(survivaltime_list) / pogingen
-    min_st = min(survivaltime_list)
+        best_attempt, max_st = 1, survivaltime_list[0]
+        for i, v in enumerate(survivaltime_list, start=1):
+            if v >= max_st:
+                best_attempt, max_st = i, v
 
-    best_attempt, max_st = 1, survivaltime_list[0]
-    for i, v in enumerate(survivaltime_list):
-        if v >= max_st:
-            best_attempt, max_st = i, v
-
-    print(f"attempts: {pogingen}, average survival time: {gemiddelde_st}, best attempt: attempt {best_attempt} survival time {max_st})")
+        print(f"attempts: {pogingen}, average survival time: {gemiddelde_st}, best attempt: attempt {best_attempt} survival time {max_st})")
